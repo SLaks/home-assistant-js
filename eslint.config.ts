18e970c0f24bf003acf4e1dbe5fe454e75c87a61
SLaks <Dev@SLaks.net>
2024-09-02 21:03:32 -0400

    Set up ESLint

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import globals from "globals";
+import * as pluginLit from "eslint-plugin-lit";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import type { Linter } from "eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+
+  pluginJs.configs.recommended,
+  pluginLit.configs["flat/all"],
+  ...tseslint.configs.recommended,
+  { rules: { "lit/no-template-map": "off" } }
+] satisfies Linter.FlatConfig[];
