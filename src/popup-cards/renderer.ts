1425c242895ad6b2e139d4a674bcb0e0e7ee0532
SLaks <Dev@SLaks.net>
2024-09-08 22:01:02 -0400

    Split popup cards into 2 files

--- src/popup-cards/renderer.ts
+++ src/popup-cards/renderer.ts
@@ -2,11 +2,10 @@ import {
   HomeAssistant,
   LovelaceCard,
   LovelaceConfig,
-} from "custom-card-helpers/dist/types";
-import { bindEntity, SimpleEntityBasedElement } from "./base-elements.ts";
-import { LitElement, html, css, PropertyValues } from "lit";
+} from "custom-card-helpers";
+import { css, html, LitElement, PropertyValues } from "lit";
 import { property, state } from "lit/decorators.js";
-import { CardHelpers } from "./types.ts";
+import { CardHelpers } from "../types";
 
 /** Renders a list of entity IDs as popup cards */
 class PopupCardRendererElement extends LitElement {
@@ -145,124 +144,3 @@ class PopupCardRendererElement extends LitElement {
   }
 }
 customElements.define("popup-card-renderer", PopupCardRendererElement);
-
-class PopupCardRunnerElement extends SimpleEntityBasedElement {
-  @property({ attribute: false })
-  @bindEntity({ entityId: "sensor.dashboard_alerts" })
-  cardEntities = "[]";
-
-  @state()
-  reopenDelayMs = 0;
-
-  @state()
-  browserIds?: Set<string>;
-
-  @state()
-  private editMode = false;
-
-  @state()
-  private isOpen = false;
-
-  override shouldUpdate(): boolean {
-    return true; // Always update, in case cards consume other entities.
-  }
-  override willUpdate(changedProps: PropertyValues<this>): void {
-    const previousEntities = this.cardEntities;
-    super.willUpdate(changedProps);
-    if (previousEntities !== this.cardEntities && this.cardEntities !== "[]")
-      this.isOpen = true;
-  }
-
-  setConfig(config: Record<string, unknown>) {
-    this.reopenDelayMs = parseInt(config.reopen_delay_ms as string) ?? 0;
-    this.browserIds = config.browser_ids
-      ? new Set(config.browser_ids as string[])
-      : undefined;
-  }
-  async connectedCallback() {
-    super.connectedCallback();
-
-    if (this.parentElement?.localName === "hui-card-preview") {
-      this.editMode = true;
-    }
-  }
-
-  static styles = css`
-    :host {
-      --mdc-dialog-min-width: 30px;
-      --mdc-dialog-max-width: 90vw;
-    }
-  `;
-
-  override render() {
-    if (this.editMode) {
-      return html`<ha-card style="padding: 12px;">
-        This invisible card shows popups when there are popup cards.
-      </ha-card>`;
-    }
-    const entities: string[] = JSON.parse(this.cardEntities);
-    if (!(this.browserIds?.has(window.browser_mod?.browserID ?? "") ?? true))
-      return html`<div></div>`;
-    if (!this.hass) return html`<div></div>`;
-    return html`<ha-dialog
-      ?open=${this.isOpen}
-      @closed=${this.onClosed}
-      hideActions
-    >
-      <div class="content" dialogInitialFocus>
-        <popup-card-renderer
-          .cardEntities=${entities}
-          .hass=${this.hass}
-          @card-transitioned=${this.onCardHidden}
-        >
-        </popup-card-renderer>
-      </div>
-    </ha-dialog>`;
-  }
-
-  onCardHidden() {
-    if (this.cardEntities === "[]") this.isOpen = false;
-  }
-
-  onClosed() {
-    this.isOpen = false;
-    if (!this.reopenDelayMs || this.cardEntities === "[]") return;
-    setTimeout(() => {
-      if (this.cardEntities !== "[]") this.isOpen = true;
-    }, this.reopenDelayMs);
-  }
-}
-customElements.define("popup-card-runner", PopupCardRunnerElement);
-window.customCards ??= [];
-window.customCards.push({
-  type: "popup-card-runner",
-  name: "Popup Card Runner",
-  description: "Automatically opens a popup with active popup cards.",
-});
-
-class ManualPopupCardsElement extends LitElement {
-  @property({ attribute: false })
-  hass?: HomeAssistant;
-
-  @state()
-  cardEntities: string[] = [];
-
-  async setConfig(config: { entities: string[] }) {
-    if (!config.entities) throw new Error("Please specify entities");
-    this.cardEntities = config.entities;
-  }
-  render() {
-    return html`<popup-card-renderer
-      .hass=${this.hass}
-      .cardEntities=${this.cardEntities}
-    ></popup-card-renderer>`;
-  }
-}
-
-customElements.define("manual-popup-cards", ManualPopupCardsElement);
-
-declare global {
-  interface Window {
-    browser_mod?: { browserID: string };
-  }
-}
