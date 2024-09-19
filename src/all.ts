223f6e3812feb37b60cf0890456a22c137eef449
SLaks <Dev@SLaks.net>
2024-09-18 20:37:21 -0400

    Add dev mode warning

--- src/all.ts
+++ src/all.ts
@@ -1,2 +1,24 @@
 import "./supper/supper-ui";
 import "./popup-cards/runner";
+import { render } from "lit";
+import { html } from "lit/static-html.js";
+
+if (import.meta.env.DEV) {
+  render(
+    html`<div
+      style="
+        position: fixed;
+        z-index: 999999;
+        right: 0;
+        top: 0;
+        color: red;
+        font-size: 24px;
+        text-shadow: 2px 2px 2px rgb(255 255 255 / 30%);
+        pointer-events: none;
+        font-weight: bold;"
+    >
+      SLaks UI: Local Dev!
+    </div>`,
+    document.body,
+  );
+}
